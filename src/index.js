bbad4de208f81d3f5fca61f16e300e7f7e97fc21
Timofey Chertkov <timofeychertkov@gmail.com>
2022-02-20 15:42:54 +0300

    feat: done code js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+      let stack = [];
+  for (let bracket of str) {
+          let brIndex = brackets.indexOf(bracket)
+
+          if (brIndex % 2 === 0) {
+              
+              if (bracket === brackets[brIndex + 1] && stack[stack.length - 1] === brIndex){
+                  stack.pop();
+              } else if (bracket === brackets[brIndex + 1] && stack[stack.length - 1] !== brIndex) {
+                  stack.push(brIndex)
+              }
+              else{
+                  stack.push(brIndex)
+              }
+          } 
+          else {
+              if (stack.pop() !== brIndex - 1){
+                  return false;
+              }
+          }
+          
+      }
+      return stack.length === 0
+  
+  
 }
